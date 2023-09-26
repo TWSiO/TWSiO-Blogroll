607b97e27a8a5d4a936bff7229bdb54d1c239519
TWSiO <TWSiO@protonmail.com>
2023-09-26 16:38:06 -0700

    Finishing up first draft and fixing some bugs.

--- src/blogroll_script/core.clj
+++ src/blogroll_script/core.clj
@@ -1,6 +1,6 @@
-(ns other-sites.core
+(ns blogroll-script.core
   (:gen-class
-    :methods [^:static [handler [String] String]]
+    :methods [^:static [handler [Object] String]]
     )
   (:require [clj-http.client :as http]
             [clojure.data.xml :as xml]
@@ -33,16 +33,20 @@
      (as-> secrets-endpoint X
        (http/get X {:headers headers})
        (:body X)
-    ))
+    )))
 
-(defn process-atom-entry
+(defn parse-atom-entry
   [entry]
   (let [subtags @(:children entry)
         subtag-get (fn [tag-name subtag] (as-> subtag X
                (filter #(= tag-name (:tag %)) X)
                (first X)))
         subtag-text #(:text (subtag-get %1 %2))
-        date (java.time.LocalDateTime/parse (subtag-text :updated subtags) java.time.format.DateTimeFormatter/ISO_OFFSET_DATE_TIME)
+        dt-format java.time.format.DateTimeFormatter/ISO_OFFSET_DATE_TIME
+
+        date (java.time.LocalDateTime/parse
+               (subtag-text :updated subtags)
+               dt-format)
         ]
     {:date date
      :link (:href (:attrs (subtag-get :link subtags)))
@@ -50,23 +54,26 @@
      }
    ))
 
-(defn process-atom
+(defn parse-atom
   [atom-string]
   (let [entries (xpath/$x "/feed/entry" atom-string)
-        date-titles (map process-atom-entry entries)
+        date-titles (map parse-atom-entry entries)
         ]
     (sort-by :date #(- (compare %1 %2)) date-titles)
    ))
 
-(defn process-rss-items
+(defn parse-rss-items
   [item]
   (let [subtags @(:children item)
         subtag-get (fn [tag-name subtag] (as-> subtag X
                (filter #(= tag-name (:tag %)) X)
                (first X)))
         subtag-text #(:text (subtag-get %1 %2))
-        date (java.time.LocalDateTime/parse (subtag-text :pubDate subtags) java.time.format.DateTimeFormatter/RFC_1123_DATE_TIME)
+        dt-format java.time.format.DateTimeFormatter/RFC_1123_DATE_TIME
 
+        date (java.time.LocalDateTime/parse
+               (subtag-text :pubDate subtags)
+               dt-format)
         ]
     {:date date
      :link (subtag-text :link subtags)
@@ -74,17 +81,17 @@
      }
     ))
 
-(defn process-rss
+(defn parse-rss
   [rss-string]
   (let [items (xpath/$x "/rss/channel/item" rss-string)
-        parsed-items (map process-rss-items items)
+        parsed-items (map parse-rss-items items)
         ]
     (sort-by :date #(- (compare %1 %2)) parsed-items)
     ))
 
-(def complete-atom (comp process-atom get-body))
+(def complete-atom (comp parse-atom get-body))
 
-(def complete-rss (comp process-rss get-body))
+(def complete-rss (comp parse-rss get-body))
 
 (defn jvns
   ""
@@ -116,9 +123,9 @@
   (m/map-vals first feeds)
   )
 
-(def twsio-url "https://thiswebsiteis.online/")
+(def twsio-url "https://thiswebsiteis.online")
 
-(def twsio-blogroll (str twsio-url "/other-sites-i-follow"))
+(def twsio-blogroll (str twsio-url "/sites-i-follow.html"))
 
 (defn get-home
   []
@@ -134,6 +141,7 @@
   [aggr post]
   (let [date (:date post)
         iso-format java.time.format.DateTimeFormatter/ISO_LOCAL_DATE
+        foo (println post)
         li (str
              "<li>"
              "<time datetime=\"" (.toString date) "\">" (.format date iso-format) "</time>"
@@ -146,23 +154,53 @@
 
 (defn latest-feeds-html
   [posts]
-  (reduce latest-feeds-html-reducer "" posts)
-  )
+  (str
+    "<ol class=\"blogroll-feed\">"
+    (reduce latest-feeds-html-reducer "" posts)
+    "</ol>"
+    ))
 
 (defn home-feeds
   [html feeds]
   (let [home-template (html/html-resource (java.io.StringReader. html))
         newest-three (take 3 (combined-feeds feeds))
         feeds-html (latest-feeds-html newest-three)
-        transformed (html/at home-template [:#blogroll-feeds] (html/html-content feeds-html))
+        transformed (html/at home-template [:#blogroll-feed] (html/html-content feeds-html))
         ]
     (apply str (html/emit* transformed))
-    ;(transformed)
     ))
 
-(defn recommendations-feeds
+(def keyword-id
+  {:jvns :#jvns
+   :computer-things :#computer-things
+   })
+
+(defn blogroll-reducer
+  [template [id {:keys [date link title]}]]
+  (let [iso-format java.time.format.DateTimeFormatter/ISO_LOCAL_DATE
+        newest-html (str
+                      "<article id=\"" (subs (name id) 1) "\">"
+                      "<h4>Latest Post</h4>"
+                      "<h5><a href\"" link "\">" title "</a></h5"
+                      "<time datetime=\"" (.toString date) "\">" (.format date iso-format) "</time>"
+                      "</article>"
+                      )
+        ]
+  (html/at template [id] (html/html-content newest-html))))
+
+(defn blogroll-feeds
   [html feeds]
-  )
+  (let [blogroll-template (html/html-resource (java.io.StringReader. html))
+        latest-feeds (feeds-latest feeds)
+        match-id-post (fn [[site id]] [id (site latest-feeds)])
+        id-post (map match-id-post keyword-id)
+        ;one (println "keyword-id" keyword-id)
+        ;two (println "latest-feeds" latest-feeds)
+        ;foo (println "blogroll-template" blogroll-template)
+        ;bar (println "id-post" id-post)
+        template (reduce blogroll-reducer blogroll-template id-post)
+        ]
+    ))
 
 (defn upload-home
   [html api-key]
@@ -184,22 +222,32 @@
   "For running with the static site generator."
   [& args]
   (let [feeds (all-feeds)
-        all-latest (feeds-latest feeds)
-        newest-three (take 3 (combined-feeds feeds))
+        pre-home-html (get-home)
+        home-html (home-feeds pre-home-html feeds)
+        pre-blogroll-html (get-blogroll)
+        blogroll-html (blogroll-feeds pre-blogroll-html feeds)
         ]
-  (println newest-three)
+    (println blogroll-html)
+    (println home-html)
+    ;(println feeds)
     ))
 
 (defn -handler
-  []
+  [s]
   (let
-    [api-key (get-api-key)
+    [;api-key (get-api-key)
      feeds (all-feeds)
      pre-home-html (get-home)
      home-html (home-feeds pre-home-html feeds)
      pre-blogroll-html (get-blogroll)
      blogroll-html (blogroll-feeds pre-blogroll-html feeds)
      ]
-    (upload-home home-html)
-    (upload-blogroll blogroll-html)
+    (str "Test: \n"
+         blogroll-html
+         "\n###\n"
+         home-html)
+    ;(println blogroll-html)
+    ;(println home-html)
+    ;(upload-home home-html api-key)
+    ;(upload-blogroll blogroll-html api-key)
     ))
